318ed74a1a09a2919b7212e57bc1a18cbcb36425
sm5791 <butterflyfree922@gmail.com>
2025-06-19 18:30:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
-import matplotlib.pyplot as plt
-import math
-import numpy as np
-import pandas as pd
-from datetime import datetime
-from zoneinfo import ZoneInfo
-from sklearn.linear_model import LinearRegression
-from mpl_toolkits.mplot3d import Axes3D
-import seaborn
+import streamlit
 
 
 st.text("s")
