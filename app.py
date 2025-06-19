9716a2fd3d0624fd6db29723a09d4e863871e103
sm5791 <butterflyfree922@gmail.com>
2025-06-19 18:15:51 +0900

    g

--- app.py
+++ app.py
@@ -10,9 +10,6 @@ Original file is located at
 import streamlit as st
 import numpy as np
 import pandas as pd
-import folium
-from folium.plugins import Draw
-from streamlit_folium import st_folium
 
 
 data = {
@@ -71,14 +68,3 @@ inputText_B = st.text_input(label='Please input text', value='aaa')
 inputArea = st.text_area('Please input any strings', 'Place holder')
 
 
-
-m = folium.Map(location=[39.949610, -75.150282], zoom_start=5)
-
-# 描画プラグインを有効化
-Draw(export=True).add_to(m)
-
-# 地図データの描画
-st_data = st_folium(m, width=700, height=500)
-
-# 現在の状態を出力
-st.write(st_data)
