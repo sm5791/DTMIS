5f690daeb535b29a4d4dd61ba39e7928cec7a808
sm5791 <butterflyfree922@gmail.com>
2025-06-19 17:59:43 +0900

    fsd

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import streamlit as st
 
 st.title("🔢 足し算アプリ")
 a = st.number_input("1つ目の数", value=0)
-b = st.number_input("2つ目の数", value=0.0)
+b = st.number_input("2つ目の数", value=0)
 if st.button("計算"):
     st.success(f"結果: {a} + {b} = {a + b}")
 
